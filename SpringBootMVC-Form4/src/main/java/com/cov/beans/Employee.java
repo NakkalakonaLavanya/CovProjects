0c5aaba51a2698fa3a4039e3404c64f25776214b
lavanya.nakkalakona <lavanya.nakkalakona@LavanyaNakkalakona.covalensedigital.net>
2021-11-17 18:10:06 +0530

    Commit 7:deleted deptno in employee class Commit 8:SpringBootMvc4Apllication main class is completed

--- SpringBootMVC-Form4/src/main/java/com/cov/beans/Employee.java
+++ SpringBootMVC-Form4/src/main/java/com/cov/beans/Employee.java
@@ -14,16 +14,15 @@ public class Employee {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	int id;
 	String name;
-	int deptNo;
+
 	@ManyToOne(fetch = FetchType.LAZY, optional = false)
-   @JoinColumn(name = "departmentid", nullable = false)
+  //@JoinColumn(name = "dept_id", nullable = false)
 	 Department department;
 
-	public Employee(int id, String name, int deptNo, Department department) {
+	public Employee(int id, String name,  Department department) {
 		super();
 		this.id = id;
 		this.name = name;
-		this.deptNo = deptNo;
 		this.department = department;
 	}
 
@@ -47,15 +46,6 @@ public class Employee {
 	public void setName(String name) {
 		this.name = name;
 	}
-
-	public int getDeptNo() {
-		return deptNo;
-	}
-
-	public void setDeptNo(int deptNo) {
-		this.deptNo = deptNo;
-	}
-
 	public Department getDepartment() {
 		return department;
 	}
@@ -66,7 +56,7 @@ public class Employee {
 
 	@Override
 	public String toString() {
-		return "Employee [id=" + id + ", name=" + name + ", deptNo=" + deptNo + ", department=" + department + "]";
+		return "Employee [id=" + id + ", name=" + name + ", department=" + department + "]";
 	}
 
 }
