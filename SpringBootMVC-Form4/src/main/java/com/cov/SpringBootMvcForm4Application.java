1dc19f389a07f5e83812e4c367adcffe1cc3c918
lavanya.nakkalakona <lavanya.nakkalakona@LavanyaNakkalakona.covalensedigital.net>
2021-11-17 15:14:51 +0530

    Initial commit:SpringBootMVC-Form4 project created Commit 1:Department class in beans is completed Commit 2:DepartmentRepository interface in repository package is completed Commit 3:DepartmentService in service package is completed Commit 4:Employee class in beans is completed Commit 5:EmployeeRepository interface  in repository package is completed Commit 6:EmployeeService in service package is completed Commit 7:InavalidDepartmentIdException in exception package is completed Commit 8:InavalidEmployeeIdException in exception package is completed

---
+++ SpringBootMVC-Form4/src/main/java/com/cov/SpringBootMvcForm4Application.java
@@ -0,0 +1,13 @@
+package com.cov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcForm4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMvcForm4Application.class, args);
+	}
+
+}
---
+++ SpringBootMVC-Form4/src/main/java/com/cov/beans/Employee.java
@@ -0,0 +1,72 @@
+package com.cov.beans;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	int id;
+	String name;
+	int deptNo;
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "departmentid", nullable = false)
+	 Department department;
+
+	public Employee(int id, String name, int deptNo, Department department) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.deptNo = deptNo;
+		this.department = department;
+	}
+
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getDeptNo() {
+		return deptNo;
+	}
+
+	public void setDeptNo(int deptNo) {
+		this.deptNo = deptNo;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", deptNo=" + deptNo + ", department=" + department + "]";
+	}
+
+}
