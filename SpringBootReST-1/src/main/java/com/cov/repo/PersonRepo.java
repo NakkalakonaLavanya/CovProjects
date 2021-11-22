2d92c460b29041f943208432aabf772ec84994f8
lavanya.nakkalakona <lavanya.nakkalakona@LavanyaNakkalakona.covalensedigital.net>
2021-11-22 13:18:06 +0530

    commit 1:springBootReST-1 project created commit 2:created person class in bean package commit 3:personService class is completed in service package commit 4:personController is completed in controller package

---
+++ SpringBootReST-1/src/main/java/com/cov/repo/PersonRepo.java
@@ -0,0 +1,9 @@
+package com.cov.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cov.beans.Person;
+
+public interface PersonRepo extends JpaRepository<Person, Integer> {
+
+	}
